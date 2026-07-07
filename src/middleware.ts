5cf4737c0a30a96e801e97a4a4fc855e7d0ac99d
ZachDreamZ <xxtheshadowcarft@gmail.com>
2026-07-07 18:47:52 +0800

    fix: limit supabase middleware to dashboard

--- src/middleware.ts
+++ src/middleware.ts
@@ -5,7 +5,5 @@ export async function middleware(request: Request) {
 }
 
 export const config = {
-  matcher: [
-    '/((?!_next/static|_next/image|favicon.ico|.*\\.(?:svg|png|jpg|jpeg|gif|webp)$).*)',
-  ],
+  matcher: ['/dashboard/:path*'],
 }
